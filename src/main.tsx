514f55453dd3125b41d552ea88bca8be932e1d87
Javier Ahmad Arivery <116359418+DeJavi08@users.noreply.github.com>
2025-02-23 02:50:23 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Set page title
+document.title = "Flash Convert";
+
+// Set favicon dynamically
+const favicon = document.createElement("link");
+favicon.rel = "icon";
+favicon.href = "https://raw.githubusercontent.com/DeJavi08/YTPlaylist-DW/refs/heads/master/src/favicon-bolt.png";
+document.head.appendChild(favicon);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
